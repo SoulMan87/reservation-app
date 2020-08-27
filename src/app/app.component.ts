3b5f6cd544cdafc618a89ce883e8908a07ed046f
Jonathan <jonathanhinestroza87@gmail.com>
2020-08-26 23:28:35 -0500

    new commit from angular reactive app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reservation-app';
+
+  constructor(private http: HttpClient) {}
+
+  private baseUrl = 'http://localhost:8080';
+  private reservationUrl: string = this.baseUrl + '/room/v1/reservation';
+  rooms: Room[];
+
+  ngOnInit () {
+    this.rooms = [new Room('127', '137', '150'),
+      new Room('113', '183', '145'),
+      new Room('823', '130', '152')];
+  }
+
+}
+
+export class Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+
+  constructor(id: string, roomNumber: string, price: string) {
+    this.id = id;
+    this.roomNumber = roomNumber;
+    this.price = price;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
